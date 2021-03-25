d772a6ca05ae9c5babcd596dcca7f25c77997c1a
kevpdev <prepontkevin@gmail.com>
2021-03-25 20:38:51 +0100

    add json sector

--- guyartisan/src/app/home/search/search.component.ts
+++ guyartisan/src/app/home/search/search.component.ts
@@ -43,7 +43,11 @@ export class SearchComponent implements OnInit {
       })      
     }
 
+  }
 
+  onCitySelected(event){
+    const cityValue = event.target.innerHTML
+    this.homeSearchForm.get('searchCity').setValue(cityValue);
   }
 
 }
